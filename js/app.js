346be203f929a7145c410a85d3e2606c28b50a64
Ivan Puddighinu <97966255+Ovua@users.noreply.github.com>
2022-07-31 11:00:21 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,8 +0,0 @@
-// Ivan & Federico - importazione moduli js
-import {getTable} from './table.js';       //Alice
-import {getCalendar} from './calendar.js'; //Federico
-import {getGraph} from './graph.js';       //Francesco
-
-getTable()
-getCalendar()
-getGraph()
