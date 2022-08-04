c9f613320e641935af482a539c3cf43e0a8badb9
Ivan Puddighinu <97966255+Ovua@users.noreply.github.com>
2022-08-04 20:50:51 +0200

    Update bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -1,56 +1,54 @@
 const Discord = require('discord.js');
 const dotenv = require('dotenv')
 dotenv.config()
-const fs = require('fs');
 const fetch = require("node-fetch")
-const http = require("http")
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 const prefix = "/"
 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`) 
   })
 
-
-
-
-// funzione PLUS - informazioni e sessione help del bot 
-function Info() {
+// funzione di sicurezza sui conflitti con altri bot
+function securityMention() {
 	client.on("message",  (msg) => {
-		if (msg.content.includes("@here")              //funzione di sicurezza evito di chiamare il BOT attraverso suffissi come "here" / "everyone / reply 
+		if (msg.content.includes("@here")  //con questa funzione si evita di chiamare il BOT attraverso suffissi come "here" / "everyone / reply
 		|| msg.content.includes("@everyone") 
-		|| msg.type == "REPLY") return false;
-        const mentionBot = msg.mentions.has(client.user.id)
+		|| msg.type == "REPLY") 
+        return false;
+    })
+}
+
+securityMention()
 
 
-        const embed = new Discord.MessageEmbed()
+// funzione aggiuntiva alla task - informazioni e sessione help del bot 
+function Info() {
+const mentionBot = msg.mentions.has(client.user.id)
+const embed = new Discord.MessageEmbed()
         .setTitle('Command List ')
         .setColor('#98FB98')
         .addFields(
             {
-            name:"`- /articles ⇒ ritorna gli ID tutti gli articoli\n - /articles/authors ⇒ lista di autori degli articoli\n - /articles/{id} ⇒ ritorna il testo dell’articolo\n `",
-            value:"` - Menziona il bot '@' e inserisci /who per scoprrire chi sono \n Menziona il bot '@' e inserisci /version per scoprrire la versione del Bot `"
+name:"`  - /articles ⇒ ritorna gli ID tutti gli articoli\n - /articles/authors ⇒ lista degli autori\n - /articles/{id} ⇒ ritorna il testo dell'articolo\n `",
+value:"` Inserisci /info per visualizzare questo elenco\n Inserisci /who per scoprrire chi sono \n Inserisci /version per scoprrire la versione del Bot `"
         })
             
-           //per evitare conflitti con altri bot, le info verranno stampate solo se il bot e' menzionato 
-    if (mentionBot 
-    || mentionBot && msg.content.includes(prefix+'info')                           
-	|| mentionBot && msg.content.includes(prefix+'help')) { // due comandi info ed help 
+   //per evitare conflitti con altri bot, le info verranno stampate solo se il bot e' menzionato 
+    if (mentionBot && msg.content.includes(prefix+'help')) { // due comandi info ed help 
 			msg.reply(embed);
-
-	} else if (mentionBot && msg.content.includes(prefix+'who')) { // informazioni relative all developer
+    } else if (mentionBot && msg.content.includes(prefix+'who')) { // informazioni relative all developer
 			msg.reply('Developed by Ivan Puddighinu')
 	} else if (mentionBot && msg.content.includes(prefix+'version')) { //informazioni relative alla versione
 			msg.reply('Version: 0.1.0')
 			
 		}
-	})
-}
+	}
+
 Info()
 
-
-// App come da task //
+// bot come da task //
 
 // fetch dell'api
 fetch ('https://api.spaceflightnewsapi.net/v3/articles')
@@ -69,50 +67,31 @@ let IDs = []
 
 // => /articles/
 function articlesID() {
-    client.on("message",  async msg => {        //tutte le funzioni sono async
-        if (msg.content.includes("@here")        //per evitare conflitti con altri bot ho escluso le varie menzioni da ogni funzione
-        || msg.content.includes("@everyone")
-        || msg.type == "REPLY") return false;
-
-        if (msg.content.length <= 10 && msg.content.toLowerCase().includes(prefix+'articles')) {  //controlla la lunghezza del messaggio e nel caso di comandi maiuscoli li trasforma in minuscolo
+if (msg.content.toLowerCase() === prefix+'articles') {  //nel caso di comandi maiuscoli li trasforma in minuscolo e se sono identici
             await msg.reply('Lista degli ultimi 10 articoli:')
                 await msg.channel.send(IDs)
                     .catch(e => msg.reply("Bot Error on ID's"));
         } 
-    });
-}
+    };
 articlesID()
-
 
 // /articles/authors
 function articleAuthors() {
-    client.on("message",  async msg => {
-        if (msg.content.includes("@here") 
-        || msg.content.includes("@everyone") 
-        || msg.type == "REPLY") return false;
-                
-                   
-        if (msg.content.toLowerCase().includes(prefix+'authors')) {
-            await msg.reply('Lista degli ultimi 10 Autori:')
-            await msg.channel.send(Author)
-                .catch(e => msg.reply("Bot Error on Authors"));
-        }
-    });
-}
+    if (msg.content.toLowerCase() === prefix+'articles/authors') {
+                await msg.reply('Lista degli ultimi 10 Autori e relativi ID:')
+                await msg.channel.send(Author)
+                    .catch(e => msg.reply("Bot Error on Authors"));
+            }
+        };
 articleAuthors()
 
 
 // /articles/{id}
 function summaryID() {
-    client.on("message",  async msg => {
-    if (msg.content.includes("@here") 
-    || msg.content.includes("@everyone") 
-    || msg.type == "REPLY") return false;
-    
-    const numero = IDs.some(num => msg.content.includes(num))  //controllo se il numero e' presente all'interno dell'array IDS
+const numero = IDs.some(num => msg.content.includes(num))  //controllo se il numero e' presente all'interno dell'array IDS
          
-	  if (msg.content.length <= 15 && numero && msg.content.toLowerCase().includes(prefix+'articles')) { 
-        const args = msg.content.slice(prefix.length).trim().split(/articles/); //split articles per fetchare in maniera corretta
+	if (numero && msg.content.toLowerCase().includes(prefix+'articles')) { 
+        const args = msg.content.slice(prefix.length).trim().split(/articles/); //split articles perevitare conflitti nel fetch
         const command = args.shift();
             fetch('https://api.spaceflightnewsapi.net/v3/articles' + args)
             .then(res => res.json())
@@ -123,14 +102,11 @@ function summaryID() {
                         .catch(e => msg.reply("Bot Error on Summary"));
             }) 
 	    } 
-    });
-}
+    };
 summaryID()
 
 
 client.login(process.env.TOKEN);
   
-
-
 
 
